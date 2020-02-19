4aa50a9c4bc6b07ef2101b74cf29e3b7c7f2f7fd
victoriasviridchik <viccisviri@gmail.com>
2020-02-19 19:43:14 +0300

    qwerty

---
+++ cmake-build-debug/CMakeFiles/progalaba2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progalaba2.dir/main.c.o"
+  "progalaba2.pdb"
+  "progalaba2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/progalaba2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+
+double from_str_to_double(char s[125]) {
+    if (s[0] == '-') {
+        printf("%s", " Should be positive! EVERETHING\n");
+        return -1;
+    }
+    int i = 0, flagdot = 0;
+    double sum = 0, d = 1;
+    while (s[i] != '\0') {
+        if (s[i] != '.') {
+            if (s[i] <= '9' && s[i] >= '0' && sum < INT_MAX / 100) {
+                if (flagdot) { d *= 10; }
+                sum = sum * 10 + (s[i] - '0');
+                i++;
+            } else {
+                printf("%s", "Should be NUMBER!\n");
+                return -1;
+            }
+        } else {
+            if (flagdot == 1) {
+                return -1;
+            }
+            flagdot = 1;
+            i++;
+        }
+    }
+    if (flagdot) {
+        return sum / d;
+    }
+    return sum;
+}
+
+
+int main() {
+    double x, part, sum = 0, eps, diff;
+    int n = 0, i = 1;
+    char s_x[125], s_eps[125];
+    printf("Enter argument and infelicity\n");
+    scanf("%s %s", s_x, s_eps);
+    x = from_str_to_double(s_x);
+    eps = from_str_to_double(s_eps);
+    if(x>35){printf("Too big argument\n");
+       return 0;}
+    if (eps > 0 && eps < 1 && x != -1) {
+        diff = 1 + eps;
+        part = x;
+
+        for (i = 1; diff > eps; i++) {
+
+            sum += part;
+            part *= (-1) * x * x / (2. * i) / (2. * i + 1.);
+            diff = fabs(sin(x) - sum);
+            n++;
+        }
+        printf("%s  %lf\n", "The built-in sin: ", sin(x));
+        printf("%s %lf %s %lf %s %d", "x = ", x, " sum = ", sum, " n = ", n);
+
+
+
+
+//    0		0.001	0
+//    3.14		0.0001	0.00161324
+//    1.57		0.00001	1
+//    1.05		0.0001	0.86602
+//    2.06		0.0001	0.869296   // mine is  0.882776
+
+
+
+    } else { printf("%s", "Invalid data"); }
+    return 0;
+}
